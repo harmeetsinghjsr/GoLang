bd176d9027823b0c792146540a4311c7a1feee28
Harmeet <hs978698@gmail.com>
2024-05-05 13:59:11 +0530

    datatypes in golang

---
+++ datatypes.go
@@ -0,0 +1,15 @@
+package main
+import ("fmt")
+func main(){
+	var a bool=true
+	var b int=5
+	var c float32=5.6
+	var d string="ABC"
+	var e complex64=5i+5
+
+	fmt.Println("boolean value",a)
+	fmt.Println("integer value",b)
+	fmt.Println("float value",c)
+	fmt.Println("string value",d)
+	fmt.Println("complex value",e)
+}
