aabacebeb436b6e7d88a917284386c5eee424f5a
Harmeet <hs978698@gmail.com>
2024-05-13 13:13:30 +0530

    array in go

---
+++ array.go
@@ -0,0 +1,19 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var a = [5]int{1, 2, 3, 4, 5}
+// 	b := [3]int{6, 7, 8}
+// 	fmt.Println(a)
+// 	fmt.Println(b)
+// }
+
+package main
+import "fmt"
+func main(){
+	var a =	[5]int{1, 2, 3, 4, 5}
+	b := [3]int{6, 7, 8}
+	fmt.Println(a)
+	fmt.Println(b)
+}
