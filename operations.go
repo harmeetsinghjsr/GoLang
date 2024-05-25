a5e2579e29e2698943a3d878444879b487e9fe55
Harmeet <hs978698@gmail.com>
2024-05-25 22:44:09 +0530

    operators in golang

---
+++ operations.go
@@ -0,0 +1,18 @@
+package main
+import ("fmt")
+
+func main() {
+	// input from user
+  var a int
+  var b int
+  
+  fmt.Println(a+b)
+  fmt.Println(a-b)
+  fmt.Println(a*b)
+  fmt.Println(a/b)
+  fmt.Println(a%b)
+  fmt.Println(a)
+}
+
+
+
