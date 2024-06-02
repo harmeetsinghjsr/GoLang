a7eef3796ca34110a1d434a8607131aed530ffcf
Harmeet <hs978698@gmail.com>
2024-06-02 21:50:13 +0530

    how to modify a preexisting slice in go

---
+++ slicemodify.go
@@ -0,0 +1,24 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	// Create a slice
+// 	numbers := []int{1, 2, 3, 4, 5}
+
+// 	// Modify the slice
+// 	numbers[2] = 10
+// 	numbers = append(numbers, 6)
+
+// 	// Print the modified slice
+// 	fmt.Println(numbers)
+// }
+package main
+import ("fmt")
+
+func main() {
+  prices := []int{10,20,30}
+
+  fmt.Println(prices)
+//   fmt.Println(prices[2])
+}
