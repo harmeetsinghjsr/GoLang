4dabe9fcfef501b20ad7c171214498bee8524e99
Harmeet <hs978698@gmail.com>
2024-06-06 00:03:41 +0530

    functions in golang

---
+++ functions1.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+func sum(a int, b int){
+	fmt.Println(a+b)
+}
+func main() {
+	fmt.Println("Enter 2 numbers to add:")
+	var a, b int
+	fmt.Scanf("%d %d", &a, &b)
+	// fmt.Println("The sum of", a, "and", b, "is", sum(a, b))
+	sum(a, b)
+}
