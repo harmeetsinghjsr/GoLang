69eae029bcd84dbef938c6b9f59c66e2eba132db
Harmeet <hs978698@gmail.com>
2024-06-08 16:47:20 +0530

    recursion factorial program

---
+++ recursion2.go
@@ -0,0 +1,15 @@
+package main
+import ("fmt")
+
+func factorial_recursion(x float64) (y float64) {
+  if x > 0 {
+     y = x * factorial_recursion(x-1)
+  } else {
+     y = 1
+  }
+  return
+}
+
+func main() {
+  fmt.Println(factorial_recursion(4))
+}
