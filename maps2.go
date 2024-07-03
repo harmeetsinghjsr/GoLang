012fe5d43ea0e2d2b46b19545c9dea28b874090f
Harmeet <hs978698@gmail.com>
2024-07-04 01:04:08 +0530

    maps over

---
+++ maps2.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+func main(){
+	m := make(map[string]string)
+	m["A"]="Apple"
+	m["B"]="Ball"
+	m["C"]="Cat"
+	m["D"]="Dog"
+	for key, value := range m {
+		fmt.Println(key, value)
+		// fmt.Println(m)
+	}
+}
